b2300fd0e4f634fbd026908e4513b94c751edfcd
hungryglobe8 <casrand@gmail.com>
2020-09-30 00:07:24 -0600

    V 1.0

--- NaturalLanguageProcessing/Program2/cky.py
+++ NaturalLanguageProcessing/Program2/cky.py
@@ -66,21 +66,30 @@ class Sentence():
     def __init__(self, line):
         self.words = line.split()
 
+    def __len__(self):
+        return len(self.words)
+
+    def __str__(self):
+        res = ""
+        for word in self.words:
+            res += f"{word} "
+        return res.strip()
+
 class Rule():
     def __init__(self, line):
         tokens = line.split()
-        self.word = tokens[0]
+        self.left_side = tokens[0]
         if len(tokens) == 4:
-            self.rule = tokens[2:3]
+            self.terminals = tokens[2:3]
         else:
-            self.rule = tokens[2:4]
+            self.terminals = tokens[2:4]
         self.prob = tokens[-1]
 
     def __str__(self):
-        return f"{self.word}: {self.rule}, {self.prob}"
+        return f"{self.left_side}: {self.terminals}, {self.prob}"
 
-    def get_rule(self):
-        return self.rule
+    def get_terminals(self):
+        return self.terminals
 
 class Grammar():
     def __init__(self):
@@ -92,6 +101,81 @@ class Grammar():
     def add_rule(self, rule):
         self.rules.append(rule)
 
+    def get_rules_leading_to_word(self, right_side):
+        rules = set()
+        # Go through all rules.
+        for rule in self.rules:
+            # Check for word in terminals.
+            if right_side == rule.terminals:
+                rules.add(rule.left_side)
+
+        return rules
+    
+    def get_rules_leading_to_non_terminals(self, b_words, c_words):
+        '''
+        Get all rules of the form A -> BC for all possible combinations from
+            b_words and c_words.
+        '''
+        rules = list()
+        count = 0
+        # Go through all possible combinations of b_words and c_words, adding unique ones.
+        for b in b_words:
+            for c in c_words:
+                poss = self.get_rules_leading_to_word([b, c])
+                if len(poss) == 0:
+                    pass
+                else:
+                    count += 1
+                    for elm in poss:
+                        rules.append(elm)
+
+        return rules, count
+
+def run_cfk(grammar, sentence):
+    length = len(sentence)
+    table = [[list() for col in range(length + 1)] for row in range(length + 1)]
+    num_parses = 0
+    # Iterate over columns.
+    for c in range(1, length + 1):
+        # Add non-terminals for the current word.
+        table[c][c] = grammar.get_rules_leading_to_word(sentence.words[c - 1:c])
+        # Iterate over rows, from bottom to top.
+        for r in range(c-1, 0, -1):
+            # Explore all partionings for words 1 through c.
+            for s in range(r+1, c+1):
+                b_words = table[r][s-1]
+                c_words = table[s][c]
+                if b_words is None or c_words is None:
+                    pass
+                else:
+                    new_rules, count = grammar.get_rules_leading_to_non_terminals(b_words, c_words)
+                    if c == length and r == 1:
+                        num_parses += count
+                    table[r][c] = table[r][c] + new_rules
+
+    return table, num_parses
+
+def print_cfk(sentence, num_parses, table):
+    # Heading information.
+    print(f"PARSING SENTENCE: {str(sentence)}")
+    print(f"NUMBER OF PARSES FOUND: {num_parses}")
+    print("TABLE:")
+    # Table.
+    length = len(table[0])
+    for r in range(1, length):
+        for c in range(r, length):
+            entries = table[r][c]
+            str_result = ""
+            if len(entries) == 0:
+                str_result = "-"
+            else:
+                for entry in sorted(entries):
+                    str_result += f"{entry} "
+                str_result = str_result.strip()
+            print(f"cell[{r},{c}]: {str_result}")
+    # New line.
+    print()
+    
 def make_readable_files(feature_set, **kwargs):
     for old_file_name, words in kwargs.items():
         new_file_name = old_file_name + ".readable"
@@ -109,19 +193,14 @@ def make_feature_files(feature_set, **kwargs):
 
 def main(args):
     pcfg_file, sentences_file = parse_args(args)
-    # Save word lists.
-    grammar_rules = read_rules_from_file(pcfg_file)
+    # Read from files.
     sentences = read_sentences_from_file(sentences_file)
-    # Link files and their respective words.
-    kwargs = {pcfg_file: grammar_rules, sentences_file: sentences}
-    print(kwargs)
-    # Get feature set.
-    # partial_features = get_features(train_words, features)
-    # full_feature_set_with_ids = feature_set(partial_features)
-    # Make readable files.
-    # make_readable_files(full_feature_set_with_ids, **kwargs)
-    # # Make feature vector files.
-    # make_feature_files(full_feature_set_with_ids, **kwargs)
+    grammar = read_grammar_from_file(pcfg_file)
+    # Run cky on each sentence.
+    for sentence in sentences:
+        table, num_parses = run_cfk(grammar, sentence)
+        print_cfk(sentence, num_parses, table)
+    # print again?
 
 if (__name__ == "__main__"):
     main(sys.argv)
--- NaturalLanguageProcessing/Program2/test_cky.py
+++ NaturalLanguageProcessing/Program2/test_cky.py
@@ -20,12 +20,40 @@ def test_rule_length():
     two_rules = cky.Rule("S -> NP VP .80")
     one_rule = cky.Rule("NP -> trust .172")
 
-    assert len(two_rules.get_rule()) == 2
-    assert len(one_rule.get_rule()) == 1
+    assert len(two_rules.get_terminals()) == 2
+    assert len(one_rule.get_terminals()) == 1
 
 def test_read_grammar_from_file():
     test_file = test_folder + "pcfg-tiny.txt"
 
     grammar = cky.read_grammar_from_file(test_file)
-    
+
     assert len(grammar) == 9
+
+def test_get_rules_leading_to_word():
+    test_file = test_folder + "pcfg-tiny.txt"
+    grammar = cky.read_grammar_from_file(test_file)
+
+    non_terminals = grammar.get_rules_leading_to_word("fish")
+
+    assert len(non_terminals) == 2
+
+def test_cfk():
+    test_file = test_folder + "pcfg-tiny.txt"
+    grammar = cky.read_grammar_from_file(test_file)
+    sentence = cky.Sentence("I trust shrinks")
+
+    table, num_parses = cky.run_cfk(grammar, sentence)
+
+    return cky.print_cfk(sentence, num_parses, table)
+
+def test_example():
+    test_file = test_folder + "pcfg-example.txt"
+    grammar = cky.read_grammar_from_file(test_file)
+    sentence = cky.Sentence("book the flight to Houston")
+
+    table, num_parses = cky.run_cfk(grammar, sentence)
+
+    return cky.print_cfk(sentence, num_parses, table)
+
+test_example()
