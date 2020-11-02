32583dc837d246fcfe06a4a7f8f1ed42f4aceb13
hungryglobe8 <casrand@gmail.com>
2020-11-01 20:51:12 -0700

    Paragraph functionality.

--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/story.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/story.py
@@ -13,26 +13,54 @@ class Story():
 	def __init__(self, file_name):
 		''' 
 		Read story information from a file.
+			Headline, Date - List of strings
+			StoryID - String
+			Text - List of Paragraphs
 		'''
-		sentences = []
 		with open(file_name) as f:
 			self.headline = f.readline().split()[1:]
 			self.date = f.readline().split()[1:]
-			self.storyid = f.readline().split()[1:]
-			while(False):
-				pass
-				# line = f.readline()
-				# # Last sentence complete.
-				# if not line:
-				# 	return sentences
+			self.storyid = f.readline().split()[1]
+			# Skip three lines.
+			for _ in range(3):
+				f.readline()
+			# Get paragraphs.
+			self.paragraphs = self.split_into_paragraphs(f.readlines())
 
-				# # Sentence has words.
-				# if line != []:
-				# 	# Might not work
-				# 	if class_name is not None:
-				# 		sentences.append(class_name(line))
-				# 	else:
-				# 		sentences.append(line.strip())
+	def split_into_paragraphs(self, text):
+		''' 
+		Splits a list of sentences into paragraph sections. 
+		If a line is a newline char by itself, start a new paragraph. 
+		'''
+		paragraphs = []
+		paragraph = Paragraph()
+		for line in text:
+			# If line is empty, start a new paragraph.
+			if line == "\n":
+				paragraphs.append(paragraph)
+				paragraph = Paragraph()
+			# Line has some content besides newline char.
+			else:
+				paragraph.add_words(line)
+
+		return paragraphs
+
+class Paragraph():
+	"""
+	A paragraph is a section of text with related content.
+	Can be searched for answers.
+	"""
+	def __init__(self):
+		self.text = []
+
+	def add_words(self, line):
+		"""
+		Adds words to the list already maintained by the paragraph.
+		"""
+		self.text += line.split()
+
+	def __len__(self):
+		return len(self.text)
 
 def read_story(file_name):
 	return Story(file_name)
--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_story.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_story.py
@@ -2,9 +2,36 @@ import pytest
 import story
 test_folder = "./NaturalLanguageProcessing/QuestionAnsweringSystem/tests/"
 
-def test_read_story_headlines():
-    test_file = test_folder + "1999-W02-5.story"
+class TestStory():
+    def read_test_story_nova_scotia(self):
+        test_file = test_folder + "1999-W02-5.story"
 
-    assert story.read_story(test_file) is not None
+        return story.read_story(test_file)
 
-test_read_story_headlines()
+    def test_read_story_is_not_null(self):
+        assert self.read_test_story_nova_scotia() is not None
+
+    def test_read_story_headlines(self):
+        sut = self.read_test_story_nova_scotia()
+
+        assert sut.headline == ["Nova", "Scotia", "School", "Pumps", "Up"]
+        assert sut.date == ["January", "8,", "1999"]
+        assert sut.storyid == "1999-W02-5"
+
+class Test_Paragraph():
+    def make_paragraph(self, lines=[]):
+        ''' Returns a basic paragraph from a list of sentences. '''
+        par = story.Paragraph()
+        for line in lines:
+            par.add_words(line)
+        return par
+
+    def test_make_paragraph_is_not_null(self):
+        assert self.make_paragraph() is not None
+
+    def test_make_one_paragraph(self):
+        text = ["I went\n", "to the store\n", "to fetch water\n", "\n", "\n"]
+
+        res = self.make_paragraph(text)
+
+        assert len(res) == 8
