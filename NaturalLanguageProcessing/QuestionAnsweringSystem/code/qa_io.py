ee3583e75ea34414863d80c9d389dc193a59b273
hungryglobe8 <casrand@gmail.com>
2020-11-02 17:42:40 -0700

    Questions and Answers in proper format.

--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/qa_io.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/qa_io.py
@@ -5,16 +5,17 @@ This file maintains various types of classes in use by a QA system:
 """
 class Question():
 	"""
-	QuestionID
-	Question
-	Difficulty
+	QuestionID - string
+	Question - list of strings (original format)
+	Difficulty - string
+	Type - first question word encountered in sentence (None if not discovered)
 	"""
 	# Possibly parse these before
 	question_words = ["who", "what", "when", "where", "why", "how much", "how"]
 	secondary_questions = ["if"]
 	def __init__(self, question_id, question, difficulty):
 		self.question_id = question_id
-		self.question = question
+		self.question = question.split()
 		self.difficulty = difficulty
 		# Figure out type of question.
 		for word in self.question:
@@ -22,3 +23,23 @@ class Question():
 			if word in self.question_words:
 				self.type = word
 				return
+
+	def answer_question(self):
+		''' Returns the answer to a question. '''
+		return Answer(self.question_id, None)
+
+class Answer():
+	"""
+	Answers for questions consist of the ID and the answer itself.
+
+	String verison is printed as:
+		ID
+		Answer
+	"""
+	def __init__(self, answer_id, answer):
+		self.answer_id = answer_id
+		self.answer = answer
+
+	def __str__(self):
+		''' Leaves self.answer if the value is None. '''			
+		return f"QuestionID: {self.answer_id}\nAnswer: {self.answer}"
--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_qa_io.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_qa_io.py
@@ -9,7 +9,39 @@ class TestQuestions():
 	def test_make_basic_question_not_null(self):
 		assert self.make_basic_question() is not None
 
-	def test_make_basic_question_correct_type(self):
+	def test_basic_question_correct_params(self):
+		sut = self.make_basic_question()
+
+		assert sut.question_id == "1"
+		assert len(sut.question) == 3
+		assert sut.difficulty == "Easy"
+
+	def test_basic_question_correct_type(self):
 		sut = self.make_basic_question()
 
 		assert sut.type == "who"
+
+	def test_answer_basic_question(self):
+		sut = self.make_basic_question()
+
+		res = sut.answer_question()
+
+		assert isinstance(res, Answer)
+
+class TestAnswers():
+	def make_basic_answer(self, a_id="1", answer="Jeremy"):
+		return Answer(a_id, answer)
+
+	def test_make_basic_answer_not_null(self):
+		assert self.make_basic_answer() is not None
+
+	def test_basic_answer_correct_fields(self):
+		sut = self.make_basic_answer()
+
+		assert sut.answer_id == "1"
+		assert sut.answer == "Jeremy"
+
+	def test_basic_answer_to_string(self):
+		sut = self.make_basic_answer()
+
+		assert str(sut) == "QuestionID: 1\nAnswer: Jeremy"
