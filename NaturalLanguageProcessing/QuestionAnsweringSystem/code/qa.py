e307611998c00c0fd7002005a37c0522531a0540
hungryglobe8 <casrand@gmail.com>
2020-11-04 09:06:59 -0700

    QA tested thoroughly and split into important chunks.

--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/qa.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/qa.py
@@ -7,23 +7,39 @@ Produce an answer file for each.
 import sys, os, re, math, story
 import validate, reader
 
+def parse_file(file_name):
+	''' 
+	Divides lines of a text file into:
+		directory - first line of file should be where stories and questions are located prepended with "."
+		story ids - remaining lines of file should be story and corresponding question ids
+	'''
+	lines = reader.read_from_file(file_name)
+	return "." + lines[0], lines[1:]
+
+def formulate_response(root):
+	'''
+	Read story at root.story (happens in reader).
+	Read questions at root.questions (happens in reader).
+	Answers questions based on story text and type of question.
+	'''
+	text = story.Story(root)
+	questions = reader.read_questions(root)
+	# Answer all questions. TODO pass in text to answer_question
+	answers = [question.answer_question() for question in questions]
+	# Write answers.
+	reader.write_response_file(root, answers)
+
 def main(args):
 	# Check input file exists.
 	validate.validate_file_names(args)
 	# Get lines of input file.
-	lines = reader.read_from_file(args[1])
-	directory = "." + lines[0]
-	storyIDs = lines[1:]
-	# For each id, read story and questions.
-	for storyID in storyIDs:
+	directory, story_ids = parse_file(args[1])
+
+	for storyID in story_ids:
 		# Root is used to access .story and .questions
 		root = directory + storyID
-		text = story.Story(root)
-		questions = reader.read_questions(root)
-		# Answer all questions. TODO pass in text to answer_question
-		answers = [question.answer_question() for question in questions]
-		# Write answers.
-		reader.write_response_file(root, answers)
+		formulate_response(root)
+
 
 if (__name__ == "__main__"):
 	main(sys.argv)
--- NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_qa.py
+++ NaturalLanguageProcessing/QuestionAnsweringSystem/code/test_qa.py
@@ -1,14 +1,29 @@
 import pytest, qa, os
 test_folder = "./NaturalLanguageProcessing/QuestionAnsweringSystem/tests/"
+Singleinput_file = test_folder + "single.input"
 
 def single_input_args():
 	input_file = test_folder + "single.input"
 
 	return [None, input_file]
 
+def test_parse_file():
+	directory, story_ids = qa.parse_file(input_file)
+
+	assert os.path.exists(directory)
+	assert len(story_ids) == 2
+
+def test_write_single_response():
+	root = test_folder + "smalldevset/1999-W02-5"
+
+	qa.formulate_response(root)
+
+	assert os.path.exists(root + ".response")
+
 def test_single_input_main():
 	args = single_input_args()
 
 	qa.main(args)
 
+	assert os.path.exists(test_folder + "smalldevset/1999-W02-5.response")
 	assert os.path.exists(test_folder + "smalldevset/1999-W03-5.response")
